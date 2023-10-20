46ab94f15c9dd4114ee385584273fc896f1ade35
khietbt <khietbt@gmail.com>
2023-10-20 04:28:22 +0000

    initial commit

---
+++ src/main/java/io/github/khietbt/SesameConfigService.java
@@ -0,0 +1,14 @@
+package io.github.khietbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SesameConfigService {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SesameConfigService.class, args);
+  }
+}
